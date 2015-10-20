49cc884019cb3184021a249ad895c4427447bded
Tim Soethout <tim@glukorizon.nl>
2015-10-20 08:32:57 +0200

    Comment about @RunWith on ScalaTest

--- src/test/scala/HelloScalaTest.scala
+++ src/test/scala/HelloScalaTest.scala
@@ -3,6 +3,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, ShouldMatchers}
 import module1.HelloScala
 
+// @RunWith helps with running all tests at once in IntelliJ
 @RunWith(classOf[JUnitRunner])
 class HelloScalaTest extends FlatSpec with ShouldMatchers {
 
