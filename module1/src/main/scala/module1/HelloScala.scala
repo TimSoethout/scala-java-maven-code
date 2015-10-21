c4f2fbeaea04a23aa9fcdeb01207c78920de4805
Tim Soethout <tim@glukorizon.nl>
2015-10-21 22:44:26 +0200

    Add Scoverage + example on statement coverage

--- module1/src/main/scala/module1/HelloScala.scala
+++ module1/src/main/scala/module1/HelloScala.scala
@@ -4,7 +4,7 @@ class HelloScala {
 
   case class TryOut(some: String, fields: List[String])
 
-  def test = "Hello"
+  def test = if (true) "Hello" else "Some very elaborate scheme to get uncovered statements"
 
   def notCovered = 42
 }
